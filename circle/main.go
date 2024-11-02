287a650769f38edbdecff93e90a446ebd4c44887
Roman Viktorovich <roman.viktorovich@outlook.com>
2024-11-02 20:13:32 +0300

    test

---
+++ circle/main.go
@@ -0,0 +1,7 @@
+package circle
+
+import "math"
+
+func GetCircleArea(radius float32) float32 {
+	return math.Pi * radius * radius
+}
---
+++ rectangle/main.go
@@ -0,0 +1,6 @@
+package rectangle
+
+func GetRectangleArea(width, height float32) float32 {
+	return width * height
+}
+
